49bb1694e5c338b78e7b16bb18733266259ece77
IhahsTnahsarp <IhahsTnahsarp@gmail.com>
2016-07-23 03:07:49 +0545

    2nd commit

---
+++ test.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template,request,redirect # For flask implementation
+from pymongo import MongoClient # Database connector
+from bson.objectid import ObjectId # For ObjectId to work
+
+client = MongoClient('localhost', 27017)    #Configure the connection to the database
+db = client.camp2016    #Select the database
+todos = db.todo #Select the collection
+
+app = Flask(__name__)
+title = "TODO with Flask"
+heading = "ToDo Reminder"
+modify=ObjectId()
+
+@app.route("/")
+@app.route("/list")
+def tasks ():
+	#Display the File
+	todos_l = todos.find()
+	return render_template('index.html',todos=todos_l,t=title,h=heading)
+
+@app.route("/add")
+def add():
+	return render_template('add.html',h=heading,t=title)
+
+@app.route("/action")
+def action ():
+	#Adding a Task
+	name=request.values.get("name")
+	desc=request.values.get("desc")
+	date=request.values.get("date")
+	pr=request.values.get("pr")
+	todos.insert({ "name":name, "desc":desc, "date":date, "pr":pr })
+	return redirect("/")
+
+@app.route("/remove")
+def remove ():
+	#Deleting a Task with various references
+	key=request.values.get("_id")
+	todos.remove({"_id":ObjectId(key)})
+	return redirect("/")
+
+@app.route("/update")
+def update ():
+	global modify
+	modify=ObjectId(request.values.get("_id"))
+	return render_template('update.html',h=heading,t=title)
+
+@app.route("/action3")
+def action3 ():
+	#Updating a Task with various references
+	refer=request.values.get("refer")
+	data=request.values.get("data")
+
+	todos.update({"_id":ObjectId(modify)}, {'$set':{refer:data}})
+	return redirect("/")
+
+@app.route("/search")
+def search():
+	#Searching a Task with various references
+
+	key=request.values.get("key")
+	refer=request.values.get("refer")
+	if(key=="_id"):
+		todos_l = todos.find({refer:ObjectId(key)})
+	else:
+		todos_l = todos.find({refer:key})
+	return render_template('searchlist.html',todos=todos_l,t=title,h=heading)
+
+if __name__ == "__main__":
+    app.run(debug=True)
