4e0c7b3a9cfa276c9db748cce6dd4b7a8d84de18
Prashant Shahi <PrashantShahi567@gmail.com>
2018-10-30 13:00:03 +0530

    Changed host to support docker link

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def about():
 	return render_template('credits.html',t=title,h=heading)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 	# Careful with the debug mode..
