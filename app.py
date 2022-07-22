182cb4ef3a55ac6dd653b3921b59bd6d18709e9d
Prashant Shahi <me@prashantshahi.dev>
2022-07-23 01:41:53 +0530

    chore: dummy changes
    Signed-off-by: Prashant Shahi <me@prashantshahi.dev>

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
 	debug = False if env == 'production' else True
 	app.run(debug=True)
-	app.run(port=port, debug=debug)
 	# Careful with the debug mode..
+	app.run(port=port, debug=debug)
